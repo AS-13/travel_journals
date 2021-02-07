9ea958661e4d1116a99b7c8291a867c5d7dcbd72
Александр Севостьянов <torresanya94@gmail.com>
2021-02-07 16:13:21 +0200

    Revert "1.0"
    This reverts commit 0b864399d74db79f2e11215c6bb7b0bc0e6d67e2.

--- android/app/src/main/java/apps/as/travel_journals/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package apps.as.travel_journals;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/material/type_journal.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class TypeJournal {
-  String name, text;
-  ExactAssetImage image;
-  TypeJournal({this.name, this.image, this.text});
-}
